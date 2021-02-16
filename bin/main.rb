14be36bad301bedbd4830d0041109d8a327fe36c
ashraffares <ashraffares090@gmail.com>
2021-02-16 22:02:16 +0200

    adding the game flow

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+puts 'welcome to tic toc toe game'
 def new_player
   puts 'player one choose a symbol x or o ?'
   user_symbol = gets.chomp
@@ -11,27 +12,45 @@ def new_player
     splayers = 'x'
   else
     puts 'wrong inputs try again'
-    new_player
+    return new_player
   end
+
   puts "player one has the sign #{fplayers}"
   puts "player two has the sign #{splayers}"
   puts
 end
 
-new_player
+board = %w[1 2 3 4 5 6 7 8 9]
+def boardshap(board)
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
+end
 
-def board
-  puts ' 1 | 2 |  3'
-  puts ' 4 | 5 |  6'
-  puts ' 7 | 8 |  9'
-
-  puts 'make your move!!!!'
-  move = gets.chomp
-  if move < '9' && move > '0'
-    puts 'good choice'
-  else
-    puts 'wrong move try again'
-    board
+def user_move(board, player_sign)
+  move_counts = 1
+  while move_counts <= 9
+    puts 'Enter your move from 1 to 9'
+    moveaction = gets.chomp.to_i
+    if !(moveaction <= 9) && (!moveaction > 0)
+      puts 'wrong move out of the board where are you going!!!'
+      return user_move(board, player_sign)
+    elsif board[moveaction - 1] == 'x' || board[moveaction - 1] == 'o'
+      puts 'wrong move are you blind!!!'
+      return user_move(board, player_sign)
+    else
+      board[moveaction - 1] = player_sign
+      move_counts += 1
+      boardshap(board)
+      player_sign = if player_sign == 'x'
+                      'o'
+                    else
+                      'x'
+                    end
+    end
   end
 end
-board
+
+new_player
+boardshap(board)
+user_move(board, player_sign = 'x')
