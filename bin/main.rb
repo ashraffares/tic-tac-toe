eaeea4449fb4635ffdd458ae8ea5604a1361144c
ashraffares <ashraffares090@gmail.com>
2021-02-19 13:24:10 +0200

    adding win and draw methods

--- bin/main.rb
+++ bin/main.rb
@@ -1,83 +1,53 @@
 #!/usr/bin/env ruby
-puts 'welcome to tic toc toe game'
-def new_player
-  puts 'player one choose a symbol x or o ?'
-  user_symbol = gets.chomp
-  case user_symbol
-  when 'x'
-    fplayers = 'x'
-    splayers = 'o'
-  when 'o'
-    fplayers = 'o'
-    splayers = 'x'
-  else
-    puts 'wrong inputs try again'
-    return new_player
+
+require_relative '../lib/settings'
+require_relative '../lib/logic'
+include Player
+def players(sign)
+  state = true
+  while state
+    if Player.sign_validate(sign)
+      puts "player #{sign} make the move"
+      break
+    else
+      puts 'enter the correct value'
+      sign = gets.chomp
+    end
   end
-
-  puts "player one has the sign #{fplayers}"
-  puts "player two has the sign #{splayers}"
-  puts
+  sign
 end
 
-board = %w[1 2 3 4 5 6 7 8 9]
-def boardshap(board)
-  puts "#{board[0]} | #{board[1]} | #{board[2]}"
-  puts "#{board[3]} | #{board[4]} | #{board[5]}"
-  puts "#{board[6]} | #{board[7]} | #{board[8]}"
-end
-
-def user_move(board, player_sign)
-  move_counts = 1
-  while move_counts <= 9
-    puts 'Enter your move from 1 to 9'
-    player_turn(player_sign)
-    moveaction = get_inputs(board, player_sign)
-    board[moveaction - 1] = player_sign
-    move_counts += 1
-    boardshap(board)
-    player_sign = if player_sign == 'x'
-                    'o'
-                  else
-                    'x'
-                  end
+def move_check(move, array, var)
+  state = true
+  while state
+    if Player.move_validate(move) && Player.index_taken(move - 1, array)
+      array[move - 1] = var
+      break
+    else
+      puts 'enter the correct value'
+      move = gets.chomp.to_i
+    end
   end
 end
-
-def player_turn(player_sign)
-  if player_sign == 'o'
-    puts 'your turn second player'
-  else
-    puts 'your turn first player'
+b = Settings.new
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+puts 'choose the player (1 is x) or (2 is o)'
+sign = gets.chomp
+sign = players(sign)
+i = 0
+while i < 9
+  move = gets.chomp.to_i
+  move_check(move, array, sign)
+  puts b.draw_board(array).to_s
+  if Player.wins(array, sign)
+    puts "player #{sign} wins \u{1F911}"
+    break
   end
-end
-
-def get_inputs(board, player_sign)
-  moveaction = gets.chomp.to_i
-  if moveaction > 9 || moveaction.negative? || moveaction.zero?
-    puts 'wrong move out of the board!!!'
-    user_move(board, player_sign)
-  elsif board[moveaction - 1] == 'x' || board[moveaction - 1] == 'o'
-    puts 'wrong move already selected!!!'
-    user_move(board, player_sign)
-  else
-    moveaction
+  if i == 8
+    Player.draw
+    break
   end
+  sign = Player.flip_user(sign)
+  puts "player #{sign} make the move"
+  i += 1
 end
-
-def player_won(player_one_turn)
-  puts 'Game ended!'
-  if player_one_turn
-    puts 'Player one won!'
-  else
-    puts 'Player two won!'
-  end
-end
-
-def draw
-  puts 'Game ended!'
-  puts "It's a draw"
-end
-
-new_player
-user_move(board, 'x')
