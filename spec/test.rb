1ee47d398076e83e920ce2e893ee43646dc85df7
ashraffares <ashraffares090@gmail.com>
2021-02-22 16:47:26 +0200

    fixing syntax added expect

--- spec/test.rb
+++ spec/test.rb
@@ -6,41 +6,41 @@ player = Player.new
 describe Player do
   describe '#game_name' do
     it 'return string' do
-      player.game_name.eql?('Wlecome to tic tac toe')
+      expect(player.game_name).to eql('Wlecome to tic tac toe')
     end
   end
 
   # sign_validate
   describe '#sign_validate' do
     it 'return true or false' do
-      player.sign_validate('x').eql?(true)
-      player.sign_validate('o').eql?(true)
-      player.sign_validate('s').eql?(false)
+      expect(player.sign_validate('x')).to eql(true)
+      expect(player.sign_validate('o')).to eql(true)
+      expect(player.sign_validate('s')).to eql(false)
     end
   end
 
   # move_validate
   describe '#move_validate' do
     it 'return true or false' do
-      player.move_validate('1').eql?(true)
-      player.move_validate('10').eql?(false)
-      player.move_validate('0').eql?(false)
+      expect(player.move_validate('1')).to eql(true)
+      expect(player.move_validate('10')).to eql(false)
+      expect(player.move_validate('0')).to eql(false)
     end
   end
 
   # index_taken
   describe '#index_taken' do
     it 'return true or false' do
-      player.index_taken(0, array).eql?(true)
-      player.index_taken(3, array).eql?(true)
-      player.index_taken(5, array).eql?(false)
+      expect(player.index_taken(0, array)).to eql(false)
+      expect(player.index_taken(3, array)).to eql(false)
+      expect(player.index_taken(5, array)).to eql(true)
     end
   end
 
   describe '#wins' do
     it 'return true or false' do
-      player.wins(array, 'x').eql?(true)
-      player.wins(array, 'o').eql?(false)
+      expect(player.wins(%W[x x x], 'x')).to eql(true)
+      expect(player.wins(%W[o x x], 'o')).to eql(false)
     end
   end
 end
@@ -49,15 +49,15 @@ describe Settings do
   describe '#flip_user' do
     it 'return x or o' do
       setting = Settings.new
-      setting.flip_user('x').eql?('o')
-      setting.flip_user('o').eql?('x')
+      expect(setting.flip_user('x')).to eql('o')
+      expect(setting.flip_user('o')).to eql('x')
     end
   end
 
   describe '#draw_board' do
     it 'return board on screen' do
       setting = Settings.new
-      setting.draw_board(array).eql?('')
+      expect(setting.draw_board([1, 2, 3, 4, 5, 6, 7, 8, 9])).to eql("\n\n\n   |   |   \n 1 | 2 | 3 \n   |   |   \n---+---+---\n   |   |   \n 4 | 5 | 6 \n   |   |   \n --+---+-- \n   |   |   \n 7 | 8 | 9 \n   |   |   \n\n\n")
     end
   end
 end
